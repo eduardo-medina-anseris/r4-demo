9726585d2e20b2c24a1675b1e011b19a7a1abbf2
eduardo.medina <eduardo.medina@net-effect.com>
2025-10-27 07:58:50 -0600

    Update theme styles - 2025-10-27 07:58:50

--- blocks/acc-title/variants.js
+++ blocks/acc-title/variants.js
@@ -1,5 +1,57 @@
 /* eslint-disable */
 export default function getVariants() {
   return {
+      "large": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Large",
+        "typography": "text-h1-large",
+        "text": "This is a title",
+        "heading": "h1",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": "text-h2-large"
+      },
+      "medium": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Medium",
+        "typography": "text-h1-medium",
+        "text": "This is a title",
+        "heading": "h2",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": "text-h2-medium"
+      },
+      "small": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Small",
+        "typography": "text-h1-small",
+        "text": "This is a title",
+        "heading": "h3",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": "text-h2-small"
+      },
+      "pretitle": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Pretitle",
+        "typography": "text-h1-pretitle",
+        "text": "pretitle",
+        "heading": "h1",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": ""
+      }
   };
 }
