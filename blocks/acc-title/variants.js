a2bbe706039bc17312543e00f611b6f8f76a4c56
asruser <asruser@cm-p47002-e1212860-aem-author-7d586b9c89-rf2mg>
2025-10-28 20:39:23 +0000

    Update theme styles - 2025-10-28 20:39:23

--- blocks/acc-title/variants.js
+++ blocks/acc-title/variants.js
@@ -2,6 +2,9 @@
 export default function getVariants() {
   return {
       "large": {
+        "jcr:lastModified": "",
+        "jcr:primaryType": "nt:unstructured",
+        "sling:resourceType": "themes/components/elements/acc/title",
         "jcr:lastModifiedBy": "",
         "typographyTablet": "",
         "_charset_": "UTF-8",
@@ -9,12 +12,12 @@ export default function getVariants() {
         "componentVariantTitle": "Large",
         "typography": "text-h1-large",
         "text": "This is a title",
-        "heading": "h1",
-        "jcr:lastModified": "",
-        "jcr:primaryType": "nt:unstructured",
-        "sling:resourceType": "themes/components/elements/acc/title"
+        "heading": "h1"
       },
       "medium": {
+        "jcr:lastModified": "",
+        "jcr:primaryType": "nt:unstructured",
+        "sling:resourceType": "themes/components/elements/acc/title",
         "jcr:lastModifiedBy": "",
         "typographyTablet": "",
         "_charset_": "UTF-8",
@@ -22,12 +25,12 @@ export default function getVariants() {
         "componentVariantTitle": "Medium",
         "typography": "text-h1-medium",
         "text": "This is a title",
-        "heading": "h2",
-        "jcr:lastModified": "",
-        "jcr:primaryType": "nt:unstructured",
-        "sling:resourceType": "themes/components/elements/acc/title"
+        "heading": "h2"
       },
       "small": {
+        "jcr:lastModified": "",
+        "jcr:primaryType": "nt:unstructured",
+        "sling:resourceType": "themes/components/elements/acc/title",
         "jcr:lastModifiedBy": "",
         "typographyTablet": "",
         "_charset_": "UTF-8",
@@ -35,12 +38,12 @@ export default function getVariants() {
         "componentVariantTitle": "Small",
         "typography": "text-h1-small",
         "text": "This is a title",
-        "heading": "h3",
-        "jcr:lastModified": "",
-        "jcr:primaryType": "nt:unstructured",
-        "sling:resourceType": "themes/components/elements/acc/title"
+        "heading": "h3"
       },
       "pretitle": {
+        "jcr:lastModified": "",
+        "jcr:primaryType": "nt:unstructured",
+        "sling:resourceType": "themes/components/elements/acc/title",
         "jcr:lastModifiedBy": "",
         "typographyTablet": "",
         "_charset_": "UTF-8",
@@ -48,10 +51,7 @@ export default function getVariants() {
         "componentVariantTitle": "Pretitle",
         "typography": "text-h1-pretitle",
         "text": "pretitle",
-        "heading": "h1",
-        "jcr:lastModified": "",
-        "jcr:primaryType": "nt:unstructured",
-        "sling:resourceType": "themes/components/elements/acc/title"
+        "heading": "h1"
       },
       "highlighted_dark": {
         "jcr:primaryType": "nt:unstructured",
