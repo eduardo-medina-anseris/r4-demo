c3fe75589d7337c49abc39a9e109b2646eecc03c
eduardo.medina <eduardo.medina@net-effect.com>
2025-11-06 16:32:51 -0600

    Update theme styles - 2025-11-06 16:32:51

--- blocks/acc-separator/variants.js
+++ blocks/acc-separator/variants.js
@@ -2,26 +2,26 @@
 export default function getVariants() {
   return {
       "big": {
-        "lineColor": "",
-        "componentVariantTitle": "Big",
         "jcr:primaryType": "nt:unstructured",
-        "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": "",
         "separation": "120",
-        "jcr:lastModified": ""
+        "lineColor": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Big",
+        "lineHeight": "",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "_charset_": "UTF-8"
       },
       "medium": {
-        "lineColor": "",
-        "componentVariantTitle": "Medium",
         "jcr:primaryType": "nt:unstructured",
-        "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": "",
         "separation": "60",
-        "jcr:lastModified": ""
+        "lineColor": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Medium",
+        "lineHeight": "",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "_charset_": "UTF-8"
       }
   };
 }
